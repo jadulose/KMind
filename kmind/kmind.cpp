247b87c44b107f932e160dd296c278c9236e5e3d
zwhsh <zhang_wh2020@163.com>
2021-09-12 22:43:53 +0800

    参照Krita改良项目结构

--- kmind/kmind.cpp
+++ kmind/kmind.cpp
@@ -8,9 +8,12 @@
 QList<Shape *> Shape::ALL_SHAPE = QList<Shape *>();
 QList<Skeleton *> Skeleton::ALL_SKELETON = QList<Skeleton *>();
 
+#include "node/k_arrange.h"
+
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    kmMainWindow mainWindow;
-    mainWindow.show();
-    return QApplication::exec();
+    //QApplication app(argc, argv);
+    //kmMainWindow mainWindow;
+    //mainWindow.show();
+    k_arrange();
+    return 0;//QApplication::exec();
 }
